691fa86a7917cb24ee88a092bda53ee109651133
TheRevGt <jgatzul@gmail.com>
2022-09-05 21:08:28 -0600

    init comit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        $user = new User();
+        $user->usuario = 'administrador';
+        $user->name = 'admin';
+        $user->password = Hash::make('admin123');
+        $user->save();
+    }
+}
